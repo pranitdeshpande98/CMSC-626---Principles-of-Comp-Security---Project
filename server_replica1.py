47d5f91486655ed6fcb27b30921fbc951efc4195
dineshgadu123 <32254503+dineshgadu123@users.noreply.github.com>
2023-04-18 01:47:41 -0400

    Create server_replica1.py
    added new file server replica1

---
+++ server_replica1.py
@@ -0,0 +1,40 @@
+import socket
+import pickle
+import os
+
+main_server = ("127.0.0.1", 65445)
+
+PORT = 65442
+
+DIR_PATH = "/Users/dineshgadu/Desktop/PCS Project1/"
+
+def handle_request(request):
+    operation, args, data = request
+    if operation == 'create':
+        filename = args
+        filepath = os.path.join(DIR_PATH, filename + ".txt")
+        if not os.path.exists(DIR_PATH):
+            os.makedirs(DIR_PATH)
+        with open(filepath, 'w') as f:
+            f.write('')
+        return "OK"
+    elif operation == 'write':
+        filename = args
+        filepath = os.path.join(DIR_PATH, filename + ".txt")
+        with open(filepath, 'a') as f:
+            f.write(data.decode('utf-8'))
+        return "file updated in replica1"
+    # Add other operations here
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("127.0.0.1", PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            request = pickle.loads(data)
+            response = handle_request(request)
+            print(response)
